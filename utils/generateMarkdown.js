1c597a7913842c3fbb960aad88716bd34ce6a4df
Diana <109758045+DianasJourney@users.noreply.github.com>
2022-11-03 14:37:35 -0400

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   let badge ='';
   if(license === 'MIT') {
@@ -10,8 +8,6 @@ function renderLicenseBadge(license) {
   return badge;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   let licenseLink = '';
   if(license === 'MIT') {
@@ -22,8 +18,6 @@ function renderLicenseLink(license) {
   return licenseLink;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   let licenseSection = '';
   if(license === 'None') {
@@ -36,7 +30,7 @@ function renderLicenseSection(license) {
   return licenseSection;
 }
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
    return `# <p align="center"> ${data.title} </p>
 
